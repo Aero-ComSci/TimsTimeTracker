b03926551b28881177e4afea381edfc6512da125
Sophie Le <sophiele@sophies-imac.lan>
2024-07-02 20:54:01 -0700

    Added more code; currently working on class inheritance, implementing get, set, and delete methods, and using super(). (Whether abstract methods will be used is debatable)

--- IssacsInstruments.py
+++ IssacsInstruments.py
@@ -30,7 +30,49 @@ default_instruments = {
 
 class Instrument():
     #Dunder method
-    def __init__(self, name, category, price):
-        name = self.name
+    global instrument_list
+    instrument_list = []
+    def __init__(self, instruments, category, price):
+        instruments = self.instruments
         category = self.category
         price = self.price
+    def by_category(self):
+        category_checked = True
+        type_of_instrument = input("What type of instrument do you want to search for? \n [A] Woodwind \n [B] Brass \n [C] Guitar Family \n [D] Percussion \n")
+        for instrument, value in default_instruments.items():
+            if type_of_instrument.upper() == "A":
+                if default_instrument[instrument][0] == "Woodwind":
+                    instrument_list.append([instrument, value])
+            elif type_of_instrument.upper() == "B":
+                if default_instrument[instrument][0] == "Brass":
+                    instrument_list.append([instrument, value])
+            elif type_of_instrument.upper() == "C":
+                if default_instrument[instrument][0] == "Guitar Family":
+                    instrument_list.append([instrument, value])
+            elif type_of_instrument.upper() == "D":
+                if default_instrument[instrument][0] == "Percussion":
+                    instrument_list.append([instrument, value])
+            else:
+                return "That's not an option."
+        return instrument_list
+    def by_price(self):
+        price_checked = True
+        price_of_instrument = input("What is your preferred price range for your instrument? (Type your max price, and we'll go from there): \n")
+        if category_checked == False:
+            for instrument, value in default_instruments.items():
+                if default_instrument[instrument][2] <= price_of_instrument:
+                    instrument_list.append([instrument, value])
+        else:
+            for instrument in instrument_list:
+                if instrument[1][2] > price_of_instrument:
+                    instrument_list.remove(instrument)
+        return instrument_list
+    #Dunder method
+    def __repr__(self):
+        return "{}: CATEGORY: {} AVAILABILITY: {}, PRICE {}".format(self.instruments, self.category, self.price)
+
+class InstrumentSettings(Instrument):
+    def get_instrument(self):
+        super().__init__(self.name, self.category, self.price)
+        for 
+
